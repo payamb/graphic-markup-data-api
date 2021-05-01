b7a513fefadd305aa413910bca6de210aec545e0
Payam <p.babaee@live.com>
2021-05-01 15:10:13 +0100

    refactor the test to start checking for invalid routes handling

--- src/app.js
+++ src/app.js
@@ -2,8 +2,4 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8280;
 
-app.get('/', (req, res) => {
-  res.send('server is running')
-})
-
 module.exports = { app, port }
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const baseUrl = require('./utils/server');
 
 describe('Test api endpoints', () => {
-  it('get request to / should return valid response', async () => {
+  it('invalid routes should return an error', async () => {
     const res = await request(baseUrl).get('/');
 
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual('server is running');
+    expect(res.body).toEqual('invalid route');
   });
 });
