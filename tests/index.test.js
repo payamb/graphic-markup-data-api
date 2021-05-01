eb3a3e130987dff1ee40d1c2bf3014d2de9dc0a0
Payam <p.babaee@live.com>
2021-05-01 14:27:29 +0100

    Create a failing test that runs api server

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,7 @@
-describe('Test', () => {
+const request = require('supertest');
+const app = require('../server');
+
+describe('Test api endpoints', () => {
   it('should test that true === true', () => {
     expect(true).toBe(true)
   })
